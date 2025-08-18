fe170d31eff5531f8d98823337645004583587ec
Lucas Léllis <125090781+Lucas-Lellis@users.noreply.github.com>
2025-08-17 22:53:26 -0300

    feat: primeiro commit

---
+++ src/test/java/net/codejava/sprint_boot_cadastrar_usuario/SprintBootCadastrarUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package net.codejava.sprint_boot_cadastrar_usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootCadastrarUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
